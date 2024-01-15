d20d083627a453d90b2ec96095f56df81f5d6efd
yanin254417 <yanin_sa@kkumail.com>
2024-01-15 15:32:21 +0700

    firstflask

---
+++ firstflask.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template, make_response
+
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hollower():
+    return "Hello, World"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0', port=500
+    
