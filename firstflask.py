1017f0ec81a587032bc4c7abdd453dbb9a7a50ee
yanin254417 <yanin_sa@kkumail.com>
2024-01-15 16:08:38 +0700

    link with html

--- firstflask.py
+++ firstflask.py
@@ -12,6 +12,10 @@ def holloworld():
 def holloYanin():
     return "Hello, Yanin!"
 
+@app.route("/home2")
+def home2():
+    return render_template("home.html",name='yanin')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0', port=500
 
